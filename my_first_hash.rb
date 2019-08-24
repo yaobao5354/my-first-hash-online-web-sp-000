76493794fa847b9b6516181ea5b5089546bd8bf4
yaoyu bao <yaobao5354@gmail.com>
2019-08-24 05:43:02 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  your_hash = {"height" => "6 feet", "weight" => "130lbs"}
 end
 
 
